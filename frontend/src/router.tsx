89289e7988fb5b5ab6a0b4aa475afeba401f5a0f
Lagro222 <wbenkhaira@gmail.com>
2026-02-13 22:23:19 +0000

    xi7aja

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -1,8 +1,6 @@
-import React from 'react'
 import { createBrowserRouter } from 'react-router-dom'
 import Dashboard from './pages/Dashboard';
 import App from './App';
-import about from './pages/About';
 import About from './pages/About';
 export const router = createBrowserRouter([
     {
