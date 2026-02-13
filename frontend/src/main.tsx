a1d2c1a18afa7a73bd3ff29975fcad2f9f765f8e
Lagro222 <wbenkhaira@gmail.com>
2026-02-13 22:23:19 +0000

    xi7aja

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,10 @@
-import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-
 import { RouterProvider } from 'react-router-dom'
 import { router } from './router'
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  <React.StrictMode>
      <RouterProvider router={router}></RouterProvider>
-  </StrictMode>
+  </React.StrictMode>
 )
