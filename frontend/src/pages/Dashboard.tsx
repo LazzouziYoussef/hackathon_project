b1536e08d28cafb74b2d95f0b1409c017e29a951
Lagro222 <wbenkhaira@gmail.com>
2026-02-14 22:16:20 +0000

    changes in the styling

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -3,8 +3,9 @@ import { Navbar } from "../components/Navbar"
 const Dashboard = () => {
   return (
     <>
-          <div >
-            <h1 className="text-white font-bold text-[50px] p-2 pl-10 animate-slide ">Dashboard</h1>
+          <div className="w-full animate-slide-right">
+                   <h1 className="text-white font-bold text-[50px] p-2 pl-10 animate-slide ">Dashboard</h1>
+            
           </div>
     </>
   )
