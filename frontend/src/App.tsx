7a55e758e243f0fb999bbf82399d4df4e1263610
Lagro222 <wbenkhaira@gmail.com>
2026-02-15 01:22:40 +0000

    adding animation to the website

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
-import { Outlet} from 'react-router-dom'
+import {  Outlet} from 'react-router-dom'
 import { Navbar } from './components/Navbar'
 function App(){
    return(
     <>
+    
      <Navbar></Navbar>
      <Outlet></Outlet>
+
     </>
    )
 }
--- frontend/src/pages/About.tsx
+++ frontend/src/pages/About.tsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div></div>
+    <>
+    <div className="relative w-full min-h-screen px-10 pt-6 ">  
+           <div className="absolute left-[500px] top-0 w-[2px] h-[90%] bg-white slide-line-up z-[-10] "></div>
+
+      <div className="absolute left-0 top-[100px]  w-[90%] h-[1px] bg-white slide-line-left"></div>
+      <div className="absolute  top-[500px]  w-[90%] h-[1px] bg-white slide-line-right"></div>
+
+        <h1 className='text-5xl text-white font-bold left animate-slide-left '>About <span className=''>us</span></h1>
+    </div>
+    </>
   )
 }
 
