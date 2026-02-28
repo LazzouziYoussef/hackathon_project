574ba2441452714170fef9048d647ca1069ace10
zayd234917 <zaidbenkhaira@gmail.com>
2026-02-28 16:41:07 +0000

    adding metrics endpoint+changing in recommandation

--- backend/app/api/endpoints/reccomend.py
+++ backend/app/api/endpoints/reccomend.py
@@ -3,6 +3,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import text
 from backend.app.database import SessionLocal
 from backend.app.services.ml_services import MLservice
+from backend.app.crud.scalling_event import get_scalling_event
 from uuid import UUID
 
 router = APIRouter()
@@ -12,10 +13,13 @@ async def get_db_for_tennant(tenant_id: str):
         await session.execute( text(f"SET app.current_tenant_id = '{tenant_id}'"))
         yield session
 
-@router.get("/{tenant_id}")
+@router.get("/reccomend/{tenant_id}")
 async def get_recommandtion(tenant_id:UUID, db : AsyncSession = Depends(get_db_for_tennant)):
     ml_services = MLservice(db=db)
     data = await ml_services.sync_and_predict(tenant_id)
+
+    history = await get_scalling_event(db,tenant_id)
+    print("history ",history)
     print("data:",data)
-    return {"tenant_id": tenant_id, "recommendation": data}
+    return {"tenant_id": tenant_id, "recommendation": data,"history": history}
         
